62bc8e60abf7408bdae9afa7908d1d2e1b5b645b
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-10 17:28:00 -0600

    Controlers status code _id replaced by id

--- src/controlers/contacts.js
+++ src/controlers/contacts.js
@@ -3,30 +3,43 @@ const Contact = require("../models/Contact");
 const getContactById = async (req, res) => {
   const contactId = req.params.id;
   try {
-    const contact = await Contact.findById(contactId);
-    if (contact) {
+    const contactRaw = await Contact.findById(contactId).lean();
+    if (contactRaw) {
+      const contact = {
+        id: contactRaw._id,
+        ...contactRaw,
+      };
+      delete contact._id;
       return res
         .status(200)
         .json({ msg: "Contact retrieved successfully", contact });
     } else {
-      return res.status(200).json({ msg: "Contact doesn't exist" });
+      return res.status(404).json({ msg: "Contact not found" });
     }
   } catch (error) {
     console.log("Error getting contact from Database: ", error);
-    return res.status(400).json({ error: "Getting Contact Error" });
+    return res.status(500).json({ error: "Getting Contact Error" });
   }
 };
 
 const getAllContacts = async (req, res) => {
   console.log("Controler");
   try {
-    const contacts = await Contact.find();
+    const contactsRaw = await Contact.find().lean();
+    const contacts = contactsRaw.map((contact) => {
+      const newContact = {
+        id: contact._id,
+        ...contact,
+      };
+      delete newContact._id;
+      return newContact;
+    });
     return res
       .status(200)
       .json({ msg: "Contacts retrieved successfully", contacts });
   } catch (error) {
     console.log("Error getting contacts from Database: ", error);
-    return res.status(400).json({ error: "Getting Contacts Error" });
+    return res.status(500).json({ error: "Getting Contacts Error" });
   }
 };
 
