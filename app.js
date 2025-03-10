51fe16d8286b1e34ae16aaa76d9f5e8f6a6187c9
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-10 17:41:25 -0600

    api-docs route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
 
 const mongodb = require("./src/database/connect");
 const contactsRouter = require("./src/routes/contacts");
@@ -10,6 +12,7 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use("/v1/contacts", contactsRouter);
 
 mongodb.initDB((err) => {
