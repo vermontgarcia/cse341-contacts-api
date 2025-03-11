2ba15eee96457ccd3314a432ecf3234242d029d8
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-10 23:11:27 -0600

    Create, Update, and Delete controlers

--- src/controlers/contacts.js
+++ src/controlers/contacts.js
@@ -10,6 +10,9 @@ const getContactById = async (req, res) => {
         ...contactRaw,
       };
       delete contact._id;
+      delete contact.created_at;
+      delete contact.updated_at;
+      delete contact.__v;
       return res
         .status(200)
         .json({ msg: "Contact retrieved successfully", contact });
@@ -23,7 +26,6 @@ const getContactById = async (req, res) => {
 };
 
 const getAllContacts = async (req, res) => {
-  console.log("Controler");
   try {
     const contactsRaw = await Contact.find().lean();
     const contacts = contactsRaw.map((contact) => {
@@ -32,6 +34,9 @@ const getAllContacts = async (req, res) => {
         ...contact,
       };
       delete newContact._id;
+      delete newContact.created_at;
+      delete newContact.updated_at;
+      delete newContact.__v;
       return newContact;
     });
     return res
@@ -43,7 +48,76 @@ const getAllContacts = async (req, res) => {
   }
 };
 
+const createContact = async (req, res) => {
+  console.log(req.body);
+  try {
+    const contactRaw = await Contact.create(req.body);
+    console.log("HERE", contactRaw);
+    const contact = {
+      id: contactRaw._doc._id,
+      ...contactRaw._doc,
+    };
+    delete contact._id;
+    delete contact.created_at;
+    delete contact.updated_at;
+    delete contact.__v;
+    return res
+      .status(200)
+      .json({ msg: "Contact created successfully", contact });
+  } catch (error) {
+    return res.status(500).json({ error: "Creating Contact Error" });
+  }
+};
+
+const updateContact = async (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  try {
+    const contactRaw = await Contact.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    ).lean();
+    if (contactRaw) {
+      const contact = {
+        id: contactRaw._id,
+        ...contactRaw,
+      };
+      delete contact._id;
+      delete contact.created_at;
+      delete contact.updated_at;
+      delete contact.__v;
+      return res
+        .status(200)
+        .json({ msg: "Contact updated successfully", contact });
+    } else {
+      return res.status(404).json({ msg: "Contact not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Updating Contact Error" });
+  }
+};
+
+const deleteContact = async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const contact = await Contact.findByIdAndDelete(req.params.id);
+    if (contact) {
+      return res.status(200).json({ msg: "Contact deleted successfully" });
+    } else {
+      return res.status(404).json({ msg: "Contact not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Deleting Contact Error" });
+  }
+};
+
 module.exports = {
   getContactById,
   getAllContacts,
+  createContact,
+  updateContact,
+  deleteContact,
 };
