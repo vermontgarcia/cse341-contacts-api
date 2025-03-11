c306e84d640a63c09f0670913b0e32442791512f
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-10 23:10:49 -0600

    Body parser config

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
-const swaggerSpec = require("./swagger");
+const bodyParser = require("body-parser");
 
 const mongodb = require("./src/database/connect");
 const contactsRouter = require("./src/routes/contacts");
+const swaggerSpec = require("./swagger");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
 
+app.use(bodyParser.json());
+
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 app.use("/v1/contacts", contactsRouter);
 
 mongodb.initDB((err) => {
